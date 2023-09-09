bab03d562644e073cd96badda5e6947e8232cd91
Alan <86123617+SneakyB00m@users.noreply.github.com>
2023-09-09 00:03:41 -0600

    GUI

--- Project #Filter/Form1.Designer.cs
+++ Project #Filter/Form1.Designer.cs
@@ -28,12 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            filterToolStripMenuItem = new ToolStripMenuItem();
+            nameToolStripMenuItem = new ToolStripMenuItem();
+            typeToolStripMenuItem = new ToolStripMenuItem();
+            videoToolStripMenuItem = new ToolStripMenuItem();
+            nameToolStripMenuItem1 = new ToolStripMenuItem();
+            typeToolStripMenuItem1 = new ToolStripMenuItem();
+            lenghtToolStripMenuItem = new ToolStripMenuItem();
+            othersToolStripMenuItem = new ToolStripMenuItem();
+            iMGPDFToolStripMenuItem = new ToolStripMenuItem();
+            toPDFToolStripMenuItem = new ToolStripMenuItem();
+            toICOToolStripMenuItem = new ToolStripMenuItem();
+            zIPToolStripMenuItem = new ToolStripMenuItem();
+            unpackingToolStripMenuItem = new ToolStripMenuItem();
+            packToolStripMenuItem = new ToolStripMenuItem();
+            crackToolStripMenuItem = new ToolStripMenuItem();
+            downloadToolStripMenuItem1 = new ToolStripMenuItem();
+            videoToolStripMenuItem2 = new ToolStripMenuItem();
+            youTubeToolStripMenuItem = new ToolStripMenuItem();
+            filesToolStripMenuItem = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { filterToolStripMenuItem, othersToolStripMenuItem, downloadToolStripMenuItem1 });
+            resources.ApplyResources(menuStrip1, "menuStrip1");
+            menuStrip1.Name = "menuStrip1";
+            // 
+            // filterToolStripMenuItem
+            // 
+            filterToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nameToolStripMenuItem, typeToolStripMenuItem, videoToolStripMenuItem });
+            filterToolStripMenuItem.Name = "filterToolStripMenuItem";
+            resources.ApplyResources(filterToolStripMenuItem, "filterToolStripMenuItem");
+            filterToolStripMenuItem.MouseHover += filterToolStripMenuItem_MouseHover;
+            // 
+            // nameToolStripMenuItem
+            // 
+            nameToolStripMenuItem.Name = "nameToolStripMenuItem";
+            resources.ApplyResources(nameToolStripMenuItem, "nameToolStripMenuItem");
+            nameToolStripMenuItem.Click += nameToolStripMenuItem_Click;
+            // 
+            // typeToolStripMenuItem
+            // 
+            typeToolStripMenuItem.Name = "typeToolStripMenuItem";
+            resources.ApplyResources(typeToolStripMenuItem, "typeToolStripMenuItem");
+            typeToolStripMenuItem.Click += typeToolStripMenuItem_Click;
+            // 
+            // videoToolStripMenuItem
+            // 
+            videoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nameToolStripMenuItem1, typeToolStripMenuItem1, lenghtToolStripMenuItem });
+            videoToolStripMenuItem.Name = "videoToolStripMenuItem";
+            resources.ApplyResources(videoToolStripMenuItem, "videoToolStripMenuItem");
+            // 
+            // nameToolStripMenuItem1
+            // 
+            nameToolStripMenuItem1.Name = "nameToolStripMenuItem1";
+            resources.ApplyResources(nameToolStripMenuItem1, "nameToolStripMenuItem1");
+            // 
+            // typeToolStripMenuItem1
+            // 
+            typeToolStripMenuItem1.Name = "typeToolStripMenuItem1";
+            resources.ApplyResources(typeToolStripMenuItem1, "typeToolStripMenuItem1");
+            // 
+            // lenghtToolStripMenuItem
+            // 
+            lenghtToolStripMenuItem.Name = "lenghtToolStripMenuItem";
+            resources.ApplyResources(lenghtToolStripMenuItem, "lenghtToolStripMenuItem");
+            // 
+            // othersToolStripMenuItem
+            // 
+            othersToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { iMGPDFToolStripMenuItem, zIPToolStripMenuItem });
+            othersToolStripMenuItem.Name = "othersToolStripMenuItem";
+            resources.ApplyResources(othersToolStripMenuItem, "othersToolStripMenuItem");
+            // 
+            // iMGPDFToolStripMenuItem
+            // 
+            iMGPDFToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toPDFToolStripMenuItem, toICOToolStripMenuItem });
+            iMGPDFToolStripMenuItem.Name = "iMGPDFToolStripMenuItem";
+            resources.ApplyResources(iMGPDFToolStripMenuItem, "iMGPDFToolStripMenuItem");
+            // 
+            // toPDFToolStripMenuItem
+            // 
+            toPDFToolStripMenuItem.Name = "toPDFToolStripMenuItem";
+            resources.ApplyResources(toPDFToolStripMenuItem, "toPDFToolStripMenuItem");
+            // 
+            // toICOToolStripMenuItem
+            // 
+            toICOToolStripMenuItem.Name = "toICOToolStripMenuItem";
+            resources.ApplyResources(toICOToolStripMenuItem, "toICOToolStripMenuItem");
+            // 
+            // zIPToolStripMenuItem
+            // 
+            zIPToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { unpackingToolStripMenuItem, packToolStripMenuItem, crackToolStripMenuItem });
+            zIPToolStripMenuItem.Name = "zIPToolStripMenuItem";
+            resources.ApplyResources(zIPToolStripMenuItem, "zIPToolStripMenuItem");
+            // 
+            // unpackingToolStripMenuItem
+            // 
+            unpackingToolStripMenuItem.Name = "unpackingToolStripMenuItem";
+            resources.ApplyResources(unpackingToolStripMenuItem, "unpackingToolStripMenuItem");
+            // 
+            // packToolStripMenuItem
+            // 
+            packToolStripMenuItem.Name = "packToolStripMenuItem";
+            resources.ApplyResources(packToolStripMenuItem, "packToolStripMenuItem");
+            // 
+            // crackToolStripMenuItem
+            // 
+            crackToolStripMenuItem.Name = "crackToolStripMenuItem";
+            resources.ApplyResources(crackToolStripMenuItem, "crackToolStripMenuItem");
+            // 
+            // downloadToolStripMenuItem1
+            // 
+            downloadToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { videoToolStripMenuItem2, filesToolStripMenuItem });
+            downloadToolStripMenuItem1.Name = "downloadToolStripMenuItem1";
+            resources.ApplyResources(downloadToolStripMenuItem1, "downloadToolStripMenuItem1");
+            // 
+            // videoToolStripMenuItem2
+            // 
+            videoToolStripMenuItem2.DropDownItems.AddRange(new ToolStripItem[] { youTubeToolStripMenuItem });
+            videoToolStripMenuItem2.Name = "videoToolStripMenuItem2";
+            resources.ApplyResources(videoToolStripMenuItem2, "videoToolStripMenuItem2");
+            // 
+            // youTubeToolStripMenuItem
+            // 
+            youTubeToolStripMenuItem.Name = "youTubeToolStripMenuItem";
+            resources.ApplyResources(youTubeToolStripMenuItem, "youTubeToolStripMenuItem");
+            // 
+            // filesToolStripMenuItem
+            // 
+            filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            resources.ApplyResources(filesToolStripMenuItem, "filesToolStripMenuItem");
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = Properties.Resources.Martin_Berube_Animal_Sheep_256;
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem filterToolStripMenuItem;
+        private ToolStripMenuItem nameToolStripMenuItem;
+        private ToolStripMenuItem typeToolStripMenuItem;
+        private ToolStripMenuItem videoToolStripMenuItem;
+        private ToolStripMenuItem nameToolStripMenuItem1;
+        private ToolStripMenuItem typeToolStripMenuItem1;
+        private ToolStripMenuItem lenghtToolStripMenuItem;
+        private ToolStripMenuItem othersToolStripMenuItem;
+        private ToolStripMenuItem iMGPDFToolStripMenuItem;
+        private ToolStripMenuItem toPDFToolStripMenuItem;
+        private ToolStripMenuItem toICOToolStripMenuItem;
+        private ToolStripMenuItem zIPToolStripMenuItem;
+        private ToolStripMenuItem unpackingToolStripMenuItem;
+        private ToolStripMenuItem packToolStripMenuItem;
+        private ToolStripMenuItem crackToolStripMenuItem;
+        private ToolStripMenuItem downloadToolStripMenuItem1;
+        private ToolStripMenuItem videoToolStripMenuItem2;
+        private ToolStripMenuItem youTubeToolStripMenuItem;
+        private ToolStripMenuItem filesToolStripMenuItem;
+        private PictureBox pictureBox1;
     }
 }
--- Project #Filter/Form1.cs
+++ Project #Filter/Form1.cs
@@ -1,10 +1,91 @@
+using Newtonsoft.Json;
+
 namespace Project__Filter
 {
     public partial class Form1 : Form
     {
+        public string pathDir = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = "Path.json";
+
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                var data = JsonConvert.DeserializeObject<dynamic>(json);
+                pathDir = data.Directory;
+            }
+            else
+            {
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedDirectory = folderBrowserDialog.SelectedPath;
+                    // Create a new JSON file in the project directory
+                    var data = new { Directory = selectedDirectory };
+                    string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                    File.WriteAllText(path, json);
+                }
+            }
+        }
+
+        private void nameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedDirectory = folderBrowserDialog.SelectedPath;
+                string filterDirectory = Path.Combine(selectedDirectory, "filter");
+                Directory.CreateDirectory(filterDirectory);
+
+                var files = Directory.GetFiles(selectedDirectory);
+                foreach (var file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    char firstLetter = char.ToUpper(fileName[0]);
+                    string subDirectory = Path.Combine(filterDirectory, firstLetter.ToString());
+                    Directory.CreateDirectory(subDirectory);
+
+                    string destinationPath = Path.Combine(subDirectory, fileName);
+                    File.Move(file, destinationPath);
+                }
+                MessageBox.Show("\nDone");
+            }
+        }
+
+        private void filterToolStripMenuItem_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_view_sort_ascending_256;
+        }
+
+        private void typeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedDirectory = folderBrowserDialog.SelectedPath;
+                string filterDirectory = Path.Combine(selectedDirectory, "filter");
+                Directory.CreateDirectory(filterDirectory);
+
+                var files = Directory.GetFiles(selectedDirectory);
+                foreach (var file in files)
+                {
+                    string extension = Path.GetExtension(file).TrimStart('.').ToLower();
+                    string destinationDirectory = Path.Combine(filterDirectory, extension);
+                    Directory.CreateDirectory(destinationDirectory);
+
+                    string fileName = Path.GetFileName(file);
+                    string destinationPath = Path.Combine(destinationDirectory, fileName);
+                    File.Move(file, destinationPath);
+                }
+                MessageBox.Show("\nDone");
+            }
         }
     }
 }
