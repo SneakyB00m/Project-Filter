15ac5e6d8b15262a400952e7c921f7bbc833ec42
Dev-Alan-Isaac <alan_isaac_288@outlook.com>
2023-11-22 15:26:09 -0600

    New GUI

--- Project #Filter/Form1.Designer.cs
+++ Project #Filter/Form1.Designer.cs
@@ -28,12 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel_Menu = new Panel();
+            button8 = new Button();
+            button7 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            button1 = new Button();
+            panel_Accent = new Panel();
+            Panel_Banner = new Panel();
+            pictureBox1 = new PictureBox();
+            button6 = new Button();
+            panel_Menu.SuspendLayout();
+            Panel_Banner.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel_Menu
+            // 
+            panel_Menu.BackColor = Color.FromArgb(64, 64, 64);
+            panel_Menu.Controls.Add(button8);
+            panel_Menu.Controls.Add(button7);
+            panel_Menu.Controls.Add(button5);
+            panel_Menu.Controls.Add(button4);
+            panel_Menu.Controls.Add(button3);
+            panel_Menu.Controls.Add(button2);
+            panel_Menu.Controls.Add(panel1);
+            panel_Menu.Controls.Add(button1);
+            panel_Menu.Dock = DockStyle.Left;
+            panel_Menu.Location = new Point(0, 0);
+            panel_Menu.Name = "panel_Menu";
+            panel_Menu.Size = new Size(250, 683);
+            panel_Menu.TabIndex = 0;
+            // 
+            // button8
+            // 
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Image = Properties.Resources.icons8_movie_ticket_30;
+            button8.Location = new Point(207, 645);
+            button8.Name = "button8";
+            button8.Size = new Size(35, 35);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Image = Properties.Resources.icons8_help_30;
+            button7.Location = new Point(3, 645);
+            button7.Name = "button7";
+            button7.Size = new Size(35, 35);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = Color.White;
+            button5.Image = Properties.Resources.icons8_windows_30;
+            button5.Location = new Point(34, 338);
+            button5.Name = "button5";
+            button5.Size = new Size(213, 53);
+            button5.TabIndex = 5;
+            button5.Text = "    Home";
+            button5.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.White;
+            button4.Image = Properties.Resources.icons8_windows_30;
+            button4.Location = new Point(34, 262);
+            button4.Name = "button4";
+            button4.Size = new Size(213, 53);
+            button4.TabIndex = 4;
+            button4.Text = "    Home";
+            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Image = Properties.Resources.icons8_windows_30;
+            button3.Location = new Point(34, 186);
+            button3.Name = "button3";
+            button3.Size = new Size(213, 53);
+            button3.TabIndex = 3;
+            button3.Text = "    Home";
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Image = Properties.Resources.icons8_windows_30;
+            button2.Location = new Point(34, 110);
+            button2.Name = "button2";
+            button2.Size = new Size(213, 53);
+            button2.TabIndex = 2;
+            button2.Text = "    Home";
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Teal;
+            panel1.Location = new Point(3, 34);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(25, 53);
+            panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Image = Properties.Resources.icons8_windows_30;
+            button1.Location = new Point(34, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(213, 53);
+            button1.TabIndex = 0;
+            button1.Text = "    Home";
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel_Accent
+            // 
+            panel_Accent.BackColor = Color.FromArgb(0, 64, 64);
+            panel_Accent.Dock = DockStyle.Top;
+            panel_Accent.Location = new Point(250, 0);
+            panel_Accent.Name = "panel_Accent";
+            panel_Accent.Size = new Size(791, 28);
+            panel_Accent.TabIndex = 1;
+            // 
+            // Panel_Banner
+            // 
+            Panel_Banner.BackColor = Color.FromArgb(0, 64, 64);
+            Panel_Banner.Controls.Add(pictureBox1);
+            Panel_Banner.Location = new Point(281, 2);
+            Panel_Banner.Name = "Panel_Banner";
+            Panel_Banner.Size = new Size(69, 101);
+            Panel_Banner.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Martin_Berube_Animal_Sheep_256;
+            pictureBox1.Location = new Point(3, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(63, 62);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button6
+            // 
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Image = Properties.Resources.icons8_power_30;
+            button6.Location = new Point(990, 34);
+            button6.Name = "button6";
+            button6.Size = new Size(35, 35);
+            button6.TabIndex = 3;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1041, 683);
+            Controls.Add(button6);
+            Controls.Add(Panel_Banner);
+            Controls.Add(panel_Accent);
+            Controls.Add(panel_Menu);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            panel_Menu.ResumeLayout(false);
+            Panel_Banner.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel_Menu;
+        private Panel panel_Accent;
+        private Panel Panel_Banner;
+        private PictureBox pictureBox1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Panel panel1;
+        private Button button1;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
---
+++ Project #Filter/Home.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Project__Filter
+{
+    partial class Home
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 64, 64);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(773, 42);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(0, 64, 64);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 498);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(773, 42);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(64, 64, 64);
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 42);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(42, 456);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(64, 64, 64);
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(731, 42);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(42, 456);
+            panel4.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.COCOS2;
+            pictureBox1.Location = new Point(42, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(689, 456);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // Filter
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBox1);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Filter";
+            Size = new Size(773, 540);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private PictureBox pictureBox1;
+    }
+}
