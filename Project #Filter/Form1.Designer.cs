757fa8f773c5a2a80873f3b93c07c76bdca721bf
Alan <86123617+SneakyB00m@users.noreply.github.com>
2023-11-22 21:21:31 -0600

    ...

--- Project #Filter/Form1.Designer.cs
+++ Project #Filter/Form1.Designer.cs
@@ -30,13 +30,13 @@
         {
             panel_Menu = new Panel();
             button6 = new Button();
-            button_Help = new Button();
             button5 = new Button();
             button4 = new Button();
             button3 = new Button();
             button2 = new Button();
             Panel_Index = new Panel();
             button1 = new Button();
+            button_Help = new Button();
             panel_Accent = new Panel();
             Panel_Banner = new Panel();
             pictureBox1 = new PictureBox();
@@ -62,8 +62,9 @@
             panel_Menu.Controls.Add(button1);
             panel_Menu.Dock = DockStyle.Left;
             panel_Menu.Location = new Point(0, 0);
+            panel_Menu.Margin = new Padding(3, 2, 3, 2);
             panel_Menu.Name = "panel_Menu";
-            panel_Menu.Size = new Size(250, 683);
+            panel_Menu.Size = new Size(219, 512);
             panel_Menu.TabIndex = 0;
             // 
             // button6
@@ -73,25 +74,15 @@
             button6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button6.ForeColor = Color.White;
             button6.Image = Properties.Resources.icons8_fingerprint_30;
-            button6.Location = new Point(34, 334);
+            button6.Location = new Point(30, 250);
+            button6.Margin = new Padding(3, 2, 3, 2);
             button6.Name = "button6";
-            button6.Size = new Size(213, 53);
+            button6.Size = new Size(186, 40);
             button6.TabIndex = 8;
             button6.Text = "    Encipher";
             button6.TextImageRelation = TextImageRelation.ImageBeforeText;
             button6.UseVisualStyleBackColor = true;
             button6.Click += button6_Click;
-            // 
-            // button_Help
-            // 
-            button_Help.FlatAppearance.BorderSize = 0;
-            button_Help.FlatStyle = FlatStyle.Flat;
-            button_Help.Image = Properties.Resources.icons8_help_30;
-            button_Help.Location = new Point(749, 46);
-            button_Help.Name = "button_Help";
-            button_Help.Size = new Size(35, 35);
-            button_Help.TabIndex = 6;
-            button_Help.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
@@ -100,9 +91,10 @@
             button5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button5.ForeColor = Color.White;
             button5.Image = Properties.Resources.icons8_merge_30;
-            button5.Location = new Point(34, 274);
+            button5.Location = new Point(30, 206);
+            button5.Margin = new Padding(3, 2, 3, 2);
             button5.Name = "button5";
-            button5.Size = new Size(213, 53);
+            button5.Size = new Size(186, 40);
             button5.TabIndex = 5;
             button5.Text = "    Merge";
             button5.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -116,9 +108,10 @@
             button4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button4.ForeColor = Color.White;
             button4.Image = Properties.Resources.icons8_undo_30;
-            button4.Location = new Point(34, 214);
+            button4.Location = new Point(30, 160);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(213, 53);
+            button4.Size = new Size(186, 40);
             button4.TabIndex = 4;
             button4.Text = "    Extract";
             button4.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -132,9 +125,10 @@
             button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button3.ForeColor = Color.White;
             button3.Image = Properties.Resources.icons8_convert_30;
-            button3.Location = new Point(34, 154);
+            button3.Location = new Point(30, 116);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(213, 53);
+            button3.Size = new Size(186, 40);
             button3.TabIndex = 3;
             button3.Text = "    Transform";
             button3.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -148,9 +142,10 @@
             button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button2.ForeColor = Color.White;
             button2.Image = Properties.Resources.icons8_filter_30;
-            button2.Location = new Point(34, 94);
+            button2.Location = new Point(30, 70);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(213, 53);
+            button2.Size = new Size(186, 40);
             button2.TabIndex = 2;
             button2.Text = "    Filter";
             button2.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -160,9 +155,10 @@
             // Panel_Index
             // 
             Panel_Index.BackColor = Color.Teal;
-            Panel_Index.Location = new Point(3, 34);
+            Panel_Index.Location = new Point(3, 26);
+            Panel_Index.Margin = new Padding(3, 2, 3, 2);
             Panel_Index.Name = "Panel_Index";
-            Panel_Index.Size = new Size(25, 53);
+            Panel_Index.Size = new Size(22, 40);
             Panel_Index.TabIndex = 1;
             // 
             // button1
@@ -172,39 +168,55 @@
             button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.White;
             button1.Image = Properties.Resources.icons8_windows_30;
-            button1.Location = new Point(34, 34);
+            button1.Location = new Point(30, 26);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(213, 53);
+            button1.Size = new Size(186, 40);
             button1.TabIndex = 0;
             button1.Text = "    Home";
             button1.TextImageRelation = TextImageRelation.ImageBeforeText;
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click_1;
             // 
+            // button_Help
+            // 
+            button_Help.FlatAppearance.BorderSize = 0;
+            button_Help.FlatStyle = FlatStyle.Flat;
+            button_Help.Image = Properties.Resources.icons8_help_30;
+            button_Help.Location = new Point(655, 32);
+            button_Help.Margin = new Padding(3, 2, 3, 2);
+            button_Help.Name = "button_Help";
+            button_Help.Size = new Size(30, 30);
+            button_Help.TabIndex = 6;
+            button_Help.UseVisualStyleBackColor = true;
+            // 
             // panel_Accent
             // 
             panel_Accent.BackColor = Color.FromArgb(0, 64, 64);
             panel_Accent.Dock = DockStyle.Top;
-            panel_Accent.Location = new Point(250, 0);
+            panel_Accent.Location = new Point(219, 0);
+            panel_Accent.Margin = new Padding(3, 2, 3, 2);
             panel_Accent.Name = "panel_Accent";
-            panel_Accent.Size = new Size(791, 28);
+            panel_Accent.Size = new Size(692, 21);
             panel_Accent.TabIndex = 1;
             // 
             // Panel_Banner
             // 
             Panel_Banner.BackColor = Color.FromArgb(0, 64, 64);
             Panel_Banner.Controls.Add(pictureBox1);
-            Panel_Banner.Location = new Point(281, 2);
+            Panel_Banner.Location = new Point(246, 2);
+            Panel_Banner.Margin = new Padding(3, 2, 3, 2);
             Panel_Banner.Name = "Panel_Banner";
-            Panel_Banner.Size = new Size(69, 101);
+            Panel_Banner.Size = new Size(60, 76);
             Panel_Banner.TabIndex = 2;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Martin_Berube_Animal_Sheep_256;
-            pictureBox1.Location = new Point(3, 32);
+            pictureBox1.Location = new Point(3, 24);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(63, 62);
+            pictureBox1.Size = new Size(55, 46);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -215,26 +227,29 @@
             button_Exit.FlatAppearance.BorderSize = 0;
             button_Exit.FlatStyle = FlatStyle.Flat;
             button_Exit.Image = Properties.Resources.icons8_power_30;
-            button_Exit.Location = new Point(994, 46);
+            button_Exit.Location = new Point(870, 34);
+            button_Exit.Margin = new Padding(3, 2, 3, 2);
             button_Exit.Name = "button_Exit";
-            button_Exit.Size = new Size(35, 35);
+            button_Exit.Size = new Size(30, 30);
             button_Exit.TabIndex = 3;
             button_Exit.UseVisualStyleBackColor = true;
             button_Exit.Click += button_Exit_Click;
             // 
             // home1
             // 
-            home1.Location = new Point(250, 110);
+            home1.Location = new Point(219, 82);
+            home1.Margin = new Padding(3, 2, 3, 2);
             home1.Name = "home1";
-            home1.Size = new Size(791, 573);
+            home1.Size = new Size(692, 430);
             home1.TabIndex = 4;
             // 
             // filter1
             // 
             filter1.BackColor = Color.FromArgb(64, 64, 64);
-            filter1.Location = new Point(250, 110);
+            filter1.Location = new Point(219, 82);
+            filter1.Margin = new Padding(3, 2, 3, 2);
             filter1.Name = "filter1";
-            filter1.Size = new Size(791, 573);
+            filter1.Size = new Size(692, 430);
             filter1.TabIndex = 5;
             // 
             // label1
@@ -242,9 +257,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(356, 51);
+            label1.Location = new Point(312, 38);
             label1.Name = "label1";
-            label1.Size = new Size(339, 24);
+            label1.Size = new Size(274, 19);
             label1.TabIndex = 6;
             label1.Text = "TOOL KIT 2.0  BY @SneakyB00m";
             // 
@@ -253,7 +268,8 @@
             button7.FlatAppearance.BorderSize = 0;
             button7.FlatStyle = FlatStyle.Flat;
             button7.Image = Properties.Resources.icons8_github_30;
-            button7.Location = new Point(701, 48);
+            button7.Location = new Point(613, 32);
+            button7.Margin = new Padding(3, 2, 3, 2);
             button7.Name = "button7";
             button7.Size = new Size(30, 30);
             button7.TabIndex = 7;
@@ -261,10 +277,10 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
-            ClientSize = new Size(1041, 683);
+            ClientSize = new Size(911, 512);
             Controls.Add(button7);
             Controls.Add(button_Help);
             Controls.Add(label1);
@@ -275,6 +291,7 @@
             Controls.Add(panel_Accent);
             Controls.Add(panel_Menu);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             panel_Menu.ResumeLayout(false);
--- Project #Filter/obj/Debug/net6.0-windows/Project #Filter.AssemblyInfo.cs
+++ Project #Filter/obj/Debug/net6.0-windows/Project #Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f4c00c0a106a4e71bf70d4f1bc399f73392c81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
