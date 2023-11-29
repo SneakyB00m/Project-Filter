c9cfb41fbf2891f6a8dabd4dee6279189e8fa4a2
Dev-Alan-Isaac <alan_isaac_288@outlook.com>
2023-11-29 17:21:22 -0600

    ...

--- Project #Filter/obj/Debug/net6.0-windows/Project #Filter.AssemblyInfo.cs
+++ Project #Filter/obj/Debug/net6.0-windows/Project #Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e650d72abf907cf444ca674ff3dc28046a7aab6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65f58f753454d766e03be8ea6fcfae2b1be00f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
