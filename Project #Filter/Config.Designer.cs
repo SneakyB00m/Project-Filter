dd61e60012826fbdf279bca01b5e07dc545a212f
Alan <86123617+SneakyB00m@users.noreply.github.com>
2023-12-02 19:09:05 -0600

    ...

---
+++ Project #Filter/Config.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Project__Filter
+{
+    partial class Config
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox_Path = new TextBox();
+            label2 = new Label();
+            checkBox_Delete = new CheckBox();
+            button_Filter = new Button();
+            progressBar_Time = new ProgressBar();
+            label3 = new Label();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            panel2 = new Panel();
+            panel1 = new Panel();
+            button_Path = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(64, 64, 64);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(38, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 32);
+            label1.TabIndex = 34;
+            label1.Text = "Path:";
+            // 
+            // textBox_Path
+            // 
+            textBox_Path.BackColor = Color.FromArgb(64, 64, 64);
+            textBox_Path.BorderStyle = BorderStyle.None;
+            textBox_Path.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
+            textBox_Path.ForeColor = Color.White;
+            textBox_Path.Location = new Point(115, 51);
+            textBox_Path.Margin = new Padding(3, 2, 3, 2);
+            textBox_Path.Name = "textBox_Path";
+            textBox_Path.ReadOnly = true;
+            textBox_Path.Size = new Size(523, 20);
+            textBox_Path.TabIndex = 36;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(64, 64, 64);
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(41, 317);
+            label2.Name = "label2";
+            label2.Size = new Size(292, 19);
+            label2.TabIndex = 37;
+            label2.Text = "Would you like to delete the original files?";
+            // 
+            // checkBox_Delete
+            // 
+            checkBox_Delete.AutoSize = true;
+            checkBox_Delete.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            checkBox_Delete.ForeColor = Color.White;
+            checkBox_Delete.Location = new Point(348, 320);
+            checkBox_Delete.Margin = new Padding(3, 2, 3, 2);
+            checkBox_Delete.Name = "checkBox_Delete";
+            checkBox_Delete.Size = new Size(15, 14);
+            checkBox_Delete.TabIndex = 38;
+            checkBox_Delete.UseVisualStyleBackColor = true;
+            // 
+            // button_Filter
+            // 
+            button_Filter.BackColor = Color.Teal;
+            button_Filter.FlatAppearance.BorderSize = 0;
+            button_Filter.FlatStyle = FlatStyle.Flat;
+            button_Filter.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
+            button_Filter.ForeColor = Color.White;
+            button_Filter.Location = new Point(398, 313);
+            button_Filter.Margin = new Padding(3, 2, 3, 2);
+            button_Filter.Name = "button_Filter";
+            button_Filter.Size = new Size(233, 26);
+            button_Filter.TabIndex = 39;
+            button_Filter.Text = "Filter";
+            button_Filter.UseVisualStyleBackColor = false;
+            // 
+            // progressBar_Time
+            // 
+            progressBar_Time.Location = new Point(41, 347);
+            progressBar_Time.Margin = new Padding(3, 2, 3, 2);
+            progressBar_Time.Name = "progressBar_Time";
+            progressBar_Time.Size = new Size(590, 22);
+            progressBar_Time.Step = 1;
+            progressBar_Time.TabIndex = 35;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(64, 64, 64);
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(61, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(95, 30);
+            label3.TabIndex = 33;
+            label3.Text = "Options";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(64, 64, 64);
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(644, 32);
+            panel4.Margin = new Padding(3, 2, 3, 2);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(32, 341);
+            panel4.TabIndex = 32;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(64, 64, 64);
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 32);
+            panel3.Margin = new Padding(3, 2, 3, 2);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(32, 341);
+            panel3.TabIndex = 31;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(0, 64, 64);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 373);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(676, 32);
+            panel2.TabIndex = 30;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 64, 64);
+            panel1.Controls.Add(button_Path);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(676, 32);
+            panel1.TabIndex = 29;
+            // 
+            // button_Path
+            // 
+            button_Path.BackColor = Color.Teal;
+            button_Path.FlatAppearance.BorderSize = 0;
+            button_Path.FlatStyle = FlatStyle.Flat;
+            button_Path.Image = Properties.Resources.Add;
+            button_Path.Location = new Point(630, 5);
+            button_Path.Margin = new Padding(3, 2, 3, 2);
+            button_Path.Name = "button_Path";
+            button_Path.Size = new Size(44, 22);
+            button_Path.TabIndex = 0;
+            button_Path.UseVisualStyleBackColor = false;
+            // 
+            // Config
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            Controls.Add(label1);
+            Controls.Add(textBox_Path);
+            Controls.Add(label2);
+            Controls.Add(checkBox_Delete);
+            Controls.Add(button_Filter);
+            Controls.Add(progressBar_Time);
+            Controls.Add(label3);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Config";
+            Size = new Size(676, 405);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox_Path;
+        private Label label2;
+        private CheckBox checkBox_Delete;
+        private Button button_Filter;
+        private ProgressBar progressBar_Time;
+        private Label label3;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel2;
+        private Panel panel1;
+        private Button button_Path;
+    }
+}
