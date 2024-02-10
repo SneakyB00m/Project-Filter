7d4c215bcd5c81aab17757ed25885687f29e2005
Alan <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-02-10 17:35:53 -0600

    ...

--- Project #Filter/obj/Debug/net6.0-windows/Project #Filter.AssemblyInfo.cs
+++ Project #Filter/obj/Debug/net6.0-windows/Project #Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01cbeff28e0d4a5e15a87936a3e266d0e88063b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f368665143aac092ce99f46eb79141d55c27ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
