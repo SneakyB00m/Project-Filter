3178aed05320f729f0a3e163c4b46153cac311fd
Alan <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-05-01 00:48:42 -0600

    Update Opt_Sort.cs

--- Project #Filter/Opt_Sort.cs
+++ Project #Filter/Opt_Sort.cs
@@ -48,25 +48,6 @@ namespace Project__Filter
                     rootNode.Expand();
                 }
             }
-
-        }
-
-        private void PopulateTreeView(string directoryValue, TreeNode parentNode)
-        {
-            string[] directories = Directory.GetDirectories(directoryValue);
-            foreach (string directory in directories)
-            {
-                TreeNode directoryNode = new TreeNode(Path.GetFileName(directory));
-                parentNode.Nodes.Add(directoryNode);
-                PopulateTreeView(directory, directoryNode); // Recursively add subdirectories
-            }
-
-            string[] files = Directory.GetFiles(directoryValue);
-            foreach (string file in files)
-            {
-                // Add files to the TreeView
-                parentNode.Nodes.Add(new TreeNode(Path.GetFileName(file)));
-            }
         }
 
         private void button_Filter_Click(object sender, EventArgs e)
@@ -89,7 +70,8 @@ namespace Project__Filter
                         }
                         break;
                     case "Include":
-
+                        string searchText = Microsoft.VisualBasic.Interaction.InputBox("Enter the text to search for:", "Search Text", "", -1, -1);
+                        SortBySimilar(selectedPath, searchText);
                         break;
                     case "Size":
                         SortBySize(selectedPath);
@@ -101,8 +83,26 @@ namespace Project__Filter
             }
         }
 
+        private void PopulateTreeView(string directoryValue, TreeNode parentNode)
+        {
+            string[] directories = Directory.GetDirectories(directoryValue);
+            foreach (string directory in directories)
+            {
+                TreeNode directoryNode = new TreeNode(Path.GetFileName(directory));
+                parentNode.Nodes.Add(directoryNode);
+                PopulateTreeView(directory, directoryNode); // Recursively add subdirectories
+            }
+
+            string[] files = Directory.GetFiles(directoryValue);
+            foreach (string file in files)
+            {
+                // Add files to the TreeView
+                parentNode.Nodes.Add(new TreeNode(Path.GetFileName(file)));
+            }
+        }
+
         // Sorts
-        public static void SortFiles(string jsonFile, string rootPath)
+        public void SortFiles(string jsonFile, string rootPath)
         {
             // Load the JSON file
             var folders = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText(jsonFile));
@@ -132,9 +132,22 @@ namespace Project__Filter
                     }
                 }
             }
+
+            // Clear the TreeView
+            treeView1.Nodes.Clear();
+
+            // Create the root node
+            TreeNode rootNode = new TreeNode(selectedPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the TreeView with directories and files
+            PopulateTreeView(selectedPath, rootNode);
+
+            // Expand the root node
+            rootNode.Expand();
         }
 
-        public static void SortByResolution(string rootPath)
+        public void SortByResolution(string rootPath)
         {
             // Initialize FFProbe
             var ffProbe = new FFProbe();
@@ -169,9 +182,22 @@ namespace Project__Filter
                     File.Move(sortedFiles[i], newPath);
                 }
             }
+
+            // Clear the TreeView
+            treeView1.Nodes.Clear();
+
+            // Create the root node
+            TreeNode rootNode = new TreeNode(selectedPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the TreeView with directories and files
+            PopulateTreeView(selectedPath, rootNode);
+
+            // Expand the root node
+            rootNode.Expand();
         }
 
-        public static void SortByDuration(string durationFile, string rootPath)
+        public void SortByDuration(string durationFile, string rootPath)
         {
             // Load the JSON file
             var durations = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText(durationFile));
@@ -205,9 +231,55 @@ namespace Project__Filter
                     }
                 }
             }
+
+            // Clear the TreeView
+            treeView1.Nodes.Clear();
+
+            // Create the root node
+            TreeNode rootNode = new TreeNode(selectedPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the TreeView with directories and files
+            PopulateTreeView(selectedPath, rootNode);
+
+            // Expand the root node
+            rootNode.Expand();
         }
 
-        public static void SortBySize(string rootPath)
+        public void SortBySimilar(string rootPath, string searchText)
+        {
+            // Get all files in the directory and its subdirectories
+            var files = Directory.EnumerateFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+            // Filter the files by the search text
+            var similarFiles = files.Where(f => Path.GetFileNameWithoutExtension(f).ToLower().Contains(searchText.ToLower())).ToList();
+
+            // Create a new folder for the similar files
+            string similarFolder = Path.Combine(rootPath, searchText);
+            Directory.CreateDirectory(similarFolder);
+
+            // Move the similar files to the new folder
+            foreach (var file in similarFiles)
+            {
+                string destPath = Path.Combine(similarFolder, Path.GetFileName(file));
+                File.Move(file, destPath);
+            }
+
+            // Clear the TreeView
+            treeView1.Nodes.Clear();
+
+            // Create the root node
+            TreeNode rootNode = new TreeNode(selectedPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the TreeView with directories and files
+            PopulateTreeView(selectedPath, rootNode);
+
+            // Expand the root node
+            rootNode.Expand();
+        }
+
+        public void SortBySize(string rootPath)
         {
             // Get all directories
             var directories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
@@ -227,9 +299,22 @@ namespace Project__Filter
                     File.Move(sortedFiles[i], newPath);
                 }
             }
+
+            // Clear the TreeView
+            treeView1.Nodes.Clear();
+
+            // Create the root node
+            TreeNode rootNode = new TreeNode(selectedPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the TreeView with directories and files
+            PopulateTreeView(selectedPath, rootNode);
+
+            // Expand the root node
+            rootNode.Expand();
         }
 
-        public static void SortAlphabetically(string rootPath)
+        public void SortAlphabetically(string rootPath)
         {
             // Get all directories
             var directories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
@@ -249,6 +334,19 @@ namespace Project__Filter
                     System.IO.File.Move(sortedFiles[i], newPath);
                 }
             }
+
+            // Clear the TreeView
+            treeView1.Nodes.Clear();
+
+            // Create the root node
+            TreeNode rootNode = new TreeNode(selectedPath);
+            treeView1.Nodes.Add(rootNode);
+
+            // Populate the TreeView with directories and files
+            PopulateTreeView(selectedPath, rootNode);
+
+            // Expand the root node
+            rootNode.Expand();
         }
     }
 }
