585158889d0ee7ae818c573d69fc905eea06090d
Alan <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-05-05 03:22:11 -0600

    Delete spam

--- Project #Filter/obj/Debug/net8.0-windows7.0/Project #Filter.AssemblyInfo.cs
+++ Project #Filter/obj/Debug/net8.0-windows7.0/Project #Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a0af34dfd977a42035901229ad316662da741c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de35c18bc3f16e8ebf09ae7b67f8ef3b5c684412")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
