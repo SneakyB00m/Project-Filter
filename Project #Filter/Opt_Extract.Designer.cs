f747f3a13c859c5881febdcc3cc99f3a6c483a42
Dev-Alan-Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2024-06-20 16:21:53 -0600

    ...

--- Project #Filter/Opt_Extract.Designer.cs
+++ Project #Filter/Opt_Extract.Designer.cs
@@ -59,10 +59,9 @@
             panel_Path.Controls.Add(textBox_Path);
             panel_Path.Controls.Add(label1);
             panel_Path.Dock = DockStyle.Top;
-            panel_Path.Location = new Point(0, 32);
-            panel_Path.Margin = new Padding(3, 2, 3, 2);
+            panel_Path.Location = new Point(0, 43);
             panel_Path.Name = "panel_Path";
-            panel_Path.Size = new Size(667, 72);
+            panel_Path.Size = new Size(762, 96);
             panel_Path.TabIndex = 47;
             // 
             // textBox_Path
@@ -72,11 +71,10 @@
             textBox_Path.BorderStyle = BorderStyle.FixedSingle;
             textBox_Path.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold);
             textBox_Path.ForeColor = Color.White;
-            textBox_Path.Location = new Point(86, 4);
-            textBox_Path.Margin = new Padding(3, 2, 3, 2);
+            textBox_Path.Location = new Point(98, 5);
             textBox_Path.Name = "textBox_Path";
             textBox_Path.ReadOnly = true;
-            textBox_Path.Size = new Size(572, 33);
+            textBox_Path.Size = new Size(653, 39);
             textBox_Path.TabIndex = 26;
             // 
             // label1
@@ -87,7 +85,7 @@
             label1.ForeColor = Color.White;
             label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(71, 32);
+            label1.Size = new Size(89, 41);
             label1.TabIndex = 17;
             label1.Text = "Path:";
             // 
@@ -95,20 +93,18 @@
             // 
             panel_R.BackColor = Color.FromArgb(0, 64, 64);
             panel_R.Dock = DockStyle.Right;
-            panel_R.Location = new Point(667, 32);
-            panel_R.Margin = new Padding(3, 2, 3, 2);
+            panel_R.Location = new Point(762, 43);
             panel_R.Name = "panel_R";
-            panel_R.Size = new Size(32, 341);
+            panel_R.Size = new Size(37, 454);
             panel_R.TabIndex = 46;
             // 
             // panel_B
             // 
             panel_B.BackColor = Color.FromArgb(0, 64, 64);
             panel_B.Dock = DockStyle.Bottom;
-            panel_B.Location = new Point(0, 373);
-            panel_B.Margin = new Padding(3, 2, 3, 2);
+            panel_B.Location = new Point(0, 497);
             panel_B.Name = "panel_B";
-            panel_B.Size = new Size(699, 32);
+            panel_B.Size = new Size(799, 43);
             panel_B.TabIndex = 45;
             // 
             // panel_T
@@ -117,9 +113,8 @@
             panel_T.Controls.Add(button_Path);
             panel_T.Dock = DockStyle.Top;
             panel_T.Location = new Point(0, 0);
-            panel_T.Margin = new Padding(3, 2, 3, 2);
             panel_T.Name = "panel_T";
-            panel_T.Size = new Size(699, 32);
+            panel_T.Size = new Size(799, 43);
             panel_T.TabIndex = 44;
             // 
             // button_Path
@@ -130,10 +125,9 @@
             button_Path.BackgroundImageLayout = ImageLayout.Zoom;
             button_Path.FlatAppearance.BorderSize = 0;
             button_Path.FlatStyle = FlatStyle.Flat;
-            button_Path.Location = new Point(635, 4);
-            button_Path.Margin = new Padding(3, 2, 3, 2);
+            button_Path.Location = new Point(726, 5);
             button_Path.Name = "button_Path";
-            button_Path.Size = new Size(25, 25);
+            button_Path.Size = new Size(29, 33);
             button_Path.TabIndex = 1;
             button_Path.UseVisualStyleBackColor = false;
             button_Path.Click += button_Path_Click;
@@ -145,10 +139,9 @@
             panel_button.Controls.Add(checkBox_Delete);
             panel_button.Controls.Add(button_Extract);
             panel_button.Dock = DockStyle.Bottom;
-            panel_button.Location = new Point(0, 309);
-            panel_button.Margin = new Padding(3, 2, 3, 2);
+            panel_button.Location = new Point(0, 412);
             panel_button.Name = "panel_button";
-            panel_button.Size = new Size(667, 64);
+            panel_button.Size = new Size(762, 85);
             panel_button.TabIndex = 48;
             // 
             // label2
@@ -157,19 +150,18 @@
             label2.BackColor = Color.FromArgb(64, 64, 64);
             label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(3, 10);
+            label2.Location = new Point(3, 13);
             label2.Name = "label2";
-            label2.Size = new Size(307, 19);
+            label2.Size = new Size(367, 23);
             label2.TabIndex = 43;
             label2.Text = "Would you like to delete unused directories?";
             // 
             // progressBar_Time
             // 
             progressBar_Time.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            progressBar_Time.Location = new Point(3, 38);
-            progressBar_Time.Margin = new Padding(3, 2, 3, 2);
+            progressBar_Time.Location = new Point(3, 51);
             progressBar_Time.Name = "progressBar_Time";
-            progressBar_Time.Size = new Size(659, 22);
+            progressBar_Time.Size = new Size(753, 29);
             progressBar_Time.Step = 1;
             progressBar_Time.TabIndex = 42;
             // 
@@ -178,10 +170,9 @@
             checkBox_Delete.AutoSize = true;
             checkBox_Delete.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
             checkBox_Delete.ForeColor = Color.White;
-            checkBox_Delete.Location = new Point(334, 13);
-            checkBox_Delete.Margin = new Padding(3, 2, 3, 2);
+            checkBox_Delete.Location = new Point(382, 17);
             checkBox_Delete.Name = "checkBox_Delete";
-            checkBox_Delete.Size = new Size(15, 14);
+            checkBox_Delete.Size = new Size(18, 17);
             checkBox_Delete.TabIndex = 44;
             checkBox_Delete.UseVisualStyleBackColor = true;
             // 
@@ -194,10 +185,9 @@
             button_Extract.FlatStyle = FlatStyle.Flat;
             button_Extract.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold);
             button_Extract.ForeColor = Color.White;
-            button_Extract.Location = new Point(429, 6);
-            button_Extract.Margin = new Padding(3, 2, 3, 2);
+            button_Extract.Location = new Point(490, 8);
             button_Extract.Name = "button_Extract";
-            button_Extract.Size = new Size(233, 26);
+            button_Extract.Size = new Size(266, 35);
             button_Extract.TabIndex = 45;
             button_Extract.Text = "Extract";
             button_Extract.UseVisualStyleBackColor = false;
@@ -209,9 +199,10 @@
             panel1.Controls.Add(label5);
             panel1.Controls.Add(panel2);
             panel1.Dock = DockStyle.Fill;
-            panel1.Location = new Point(0, 104);
+            panel1.Location = new Point(0, 139);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(667, 205);
+            panel1.Size = new Size(762, 273);
             panel1.TabIndex = 49;
             // 
             // comboBox_Select
@@ -224,10 +215,9 @@
             comboBox_Select.ForeColor = Color.White;
             comboBox_Select.FormattingEnabled = true;
             comboBox_Select.Items.AddRange(new object[] { "Extract Files", "Uncompress RAR", "Uncompress TAR", "Uncompress ZIP" });
-            comboBox_Select.Location = new Point(26, 68);
-            comboBox_Select.Margin = new Padding(3, 2, 3, 2);
+            comboBox_Select.Location = new Point(30, 91);
             comboBox_Select.Name = "comboBox_Select";
-            comboBox_Select.Size = new Size(368, 29);
+            comboBox_Select.Size = new Size(420, 36);
             comboBox_Select.TabIndex = 42;
             // 
             // label5
@@ -236,9 +226,9 @@
             label5.BackColor = Color.FromArgb(64, 64, 64);
             label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
             label5.ForeColor = Color.White;
-            label5.Location = new Point(3, 2);
+            label5.Location = new Point(3, 7);
             label5.Name = "label5";
-            label5.Size = new Size(104, 32);
+            label5.Size = new Size(130, 41);
             label5.TabIndex = 40;
             label5.Text = "Options";
             // 
@@ -248,18 +238,18 @@
             panel2.Controls.Add(label_Count);
             panel2.Controls.Add(label4);
             panel2.Dock = DockStyle.Right;
-            panel2.Location = new Point(420, 0);
+            panel2.Location = new Point(480, 0);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(247, 205);
+            panel2.Size = new Size(282, 273);
             panel2.TabIndex = 0;
             // 
             // treeView1
             // 
             treeView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            treeView1.Location = new Point(9, 37);
-            treeView1.Margin = new Padding(3, 2, 3, 2);
+            treeView1.Location = new Point(10, 49);
             treeView1.Name = "treeView1";
-            treeView1.Size = new Size(233, 169);
+            treeView1.Size = new Size(266, 224);
             treeView1.TabIndex = 5;
             // 
             // label_Count
@@ -267,9 +257,9 @@
             label_Count.AutoSize = true;
             label_Count.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label_Count.ForeColor = Color.White;
-            label_Count.Location = new Point(144, 5);
+            label_Count.Location = new Point(165, 7);
             label_Count.Name = "label_Count";
-            label_Count.Size = new Size(67, 21);
+            label_Count.Size = new Size(84, 28);
             label_Count.TabIndex = 3;
             label_Count.Text = "File List";
             // 
@@ -278,15 +268,15 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(3, 5);
+            label4.Location = new Point(3, 7);
             label4.Name = "label4";
-            label4.Size = new Size(120, 21);
+            label4.Size = new Size(162, 28);
             label4.TabIndex = 2;
             label4.Text = "File List           #";
             // 
             // Opt_Extract
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
             Controls.Add(panel1);
@@ -295,8 +285,9 @@
             Controls.Add(panel_R);
             Controls.Add(panel_B);
             Controls.Add(panel_T);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Opt_Extract";
-            Size = new Size(699, 405);
+            Size = new Size(799, 540);
             panel_Path.ResumeLayout(false);
             panel_Path.PerformLayout();
             panel_T.ResumeLayout(false);
--- Project #Filter/obj/Debug/net8.0-windows7.0/Project #Filter.AssemblyInfo.cs
+++ Project #Filter/obj/Debug/net8.0-windows7.0/Project #Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f41616a5ee72d7f1f501100372c69e8981b83b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f8f5511d2a7f4f243234fb15b2f74e8d204c7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project #Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
